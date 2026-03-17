1b813a069bd7740bbd4a30b71b4cdd90d9f93797
CocachorroLinux <vitorhugosantosavila851@gmail.com>
2026-03-17 19:22:52 -0300

    commit

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+const formLogin = document.getElementById('login_form');
+const inputCpf = document.getElementById('cpf');
+const inputSenha = document.getElementById('senha');
+
+inputCpf.addEventListener('input', function(){
+    let valor = inputCpf.value.replace(/\D/g, '');
+
+    if(valor.length <= 11){
+        valor = valor.replace(/(\d{3})(\d)/, '$1.$2');
+        valor = valor.replace(/(\d{3})(\d)/, '$1.$2');
+        valor = valor.replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+    }
+
+    inputCpf.value=valor;
+});
+
+formLogin.addEventListener('submit', function (evento) {
+    evento.preventDefault();
+
+    const valorCpf = inputCpf.value;
+    const valorSenha = inputSenha.value;
+
+    if(valorCpf === "123" && valorSenha === "123"){
+        window.location.href = "dashboard.html";
+    } else {
+        alert("CPF ou senha incorreto. Tente novamente.")
+    }
+});
