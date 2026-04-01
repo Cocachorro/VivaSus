a360b3b6e16cb3465b19f308c83ae4cf076185d2
Cocachorro <vitorhugosantosavila851@gmail.com>
2026-04-01 11:03:45 -0300

    So formatando com prettier mesmo

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,28 +1,28 @@
-const formLogin = document.getElementById('login_form');
-const inputCpf = document.getElementById('cpf');
-const inputSenha = document.getElementById('senha');
+const formLogin = document.getElementById("login_form");
+const inputCpf = document.getElementById("cpf");
+const inputSenha = document.getElementById("senha");
 
-inputCpf.addEventListener('input', function(){
-    let valor = inputCpf.value.replace(/\D/g, '');
+inputCpf.addEventListener("input", function () {
+  let valor = inputCpf.value.replace(/\D/g, "");
 
-    if(valor.length <= 11){
-        valor = valor.replace(/(\d{3})(\d)/, '$1.$2');
-        valor = valor.replace(/(\d{3})(\d)/, '$1.$2');
-        valor = valor.replace(/(\d{3})(\d{1,2})$/, '$1-$2');
-    }
+  if (valor.length <= 11) {
+    valor = valor.replace(/(\d{3})(\d)/, "$1.$2");
+    valor = valor.replace(/(\d{3})(\d)/, "$1.$2");
+    valor = valor.replace(/(\d{3})(\d{1,2})$/, "$1-$2");
+  }
 
-    inputCpf.value=valor;
+  inputCpf.value = valor;
 });
 
-formLogin.addEventListener('submit', function (evento) {
-    evento.preventDefault();
+formLogin.addEventListener("submit", function (evento) {
+  evento.preventDefault();
 
-    const valorCpf = inputCpf.value;
-    const valorSenha = inputSenha.value;
+  const valorCpf = inputCpf.value;
+  const valorSenha = inputSenha.value;
 
-    if(valorCpf === "123" && valorSenha === "123"){
-        window.location.href = "dashboard.html";
-    } else {
-        alert("CPF ou senha incorreto. Tente novamente.")
-    }
+  if (valorCpf === "123" && valorSenha === "123") {
+    window.location.href = "dashboard.html";
+  } else {
+    alert("CPF ou senha incorreto. Tente novamente.");
+  }
 });
